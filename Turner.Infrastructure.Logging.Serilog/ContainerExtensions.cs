81fb2cb709d95cdf40d5a494bd738b1c2937bd4f
mpuneky <mpuneky@turner-industries.com>
2016-09-08 10:06:00 -0500

    feat: container extensions

---
+++ Turner.Infrastructure.Logging.Serilog/ContainerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using SimpleInjector;
+
+namespace Turner.Infrastructure.Logging.Serilog
+{
+    public static class ContainerExtensions
+    {
+        public static void ConfigureSerilog(this Container container)
+        {
+            var config = new LoggerConfiguration();
+            config.WriteTo.ColoredConsole();
+
+            ConfigureSerilog(container, config);
+        }
+
+        public static void ConfigureSerilog(this Container container, LoggerConfiguration loggerConfig)
+        {
+            container.RegisterSingleton<ILogger, SerilogAdapter>();
+            container.RegisterSingleton<global::Serilog.ILogger>(loggerConfig.CreateLogger);
+        }
+    }
+}
---
+++ Turner.Infrastructure.Logging.Serilog/SeriLogAdapter.cs
@@ -0,0 +1,22 @@
+﻿namespace Turner.Infrastructure.Logging.Serilog
+{
+    internal class SerilogAdapter : ILogger
+    {
+        private readonly global::Serilog.ILogger _logger;
+
+        public SerilogAdapter(global::Serilog.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Information(string logMessage)
+        {
+            _logger.Information(logMessage);
+        }
+
+        public void Debug(string logMessage)
+        {
+            _logger.Debug(logMessage);
+        }
+    }
+}
